53afb8483d9d24457fd7b10b7749541cbdd21cc1
NathanSub <42844708+NathanSub@users.noreply.github.com>
2018-09-27 12:41:24 -0400

    Updated from Brython Server: 9/27/2018 12:41:23 PM

--- multiplication-table.py
+++ multiplication-table.py
@@ -21,8 +21,8 @@ Height of multiplication table: 8
   7  14  21  28  35  42  49  56  63  70
   8  16  24  32  40  48  56  64  72  80
 """
-width = int(input("Width of multiplication table: "))
-height = int(input("Height of multiplication table: "))
+width = (int(input("Width of multiplication table: "))+1)
+height = (int(input("Height of multiplication table: "))+1)
 for h in range(1, height):
     for w in range(1,width):
         print("{:>3}".format(h*w), end='')
